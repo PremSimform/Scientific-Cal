f7aa9743739bdd664cdb503797b2f461051a7dc9
PremSimform <prem.m@simformsolutions.com>
2023-01-31 17:38:19 +0530

    1st Version Code

--- script.js
+++ script.js
@@ -150,3 +150,19 @@ function fact() {
 function exp() {
     ip1.value += "**"
 }
+
+
+
+
+// Loader Animation
+
+// var myVar;
+
+// function myFunction() {
+//     myVar = setTimeout(showPage, 3000);
+// }
+
+// function showPage() {
+//     document.getElementById("loader").style.display = "none";
+//     document.getElementById("myDiv").style.display = "block";
+// }
